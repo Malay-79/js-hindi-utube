708b51ff204b2ed5a428c354b376aa2c2e719423
Malay-79 <malaypratap0709@gmail.com>
2026-06-18 18:15:36 +0000

    study variable

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "asb@gmail.com"
+var accountPassword = "8765"
+accountCity="xyz"
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountState])
