196fc83fee410e8d2595a9ee016e4f10556530e2
Malay-79 <malaypratap0709@gmail.com>
2026-06-19 13:03:16 +0000

    datatypes

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -6,3 +6,4 @@ let accountState;
 
 console.log(accountId);
 console.table([accountEmail,accountPassword,accountState])
+//prefer not to use var because of issue in block scope and functional scope
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all Js code as newer version
+// alert(3+3) // we are using nodejs , not browser
+
+//code readability should be high
+let name="abc"
+let age=15
+let isloggedIn= true
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+// null=> standalone value
+//undefine=>
+//symbol=>unique
+
+//object
+console.log(typeof undefined);//undefine
+console.log(typeof null)//object
+
