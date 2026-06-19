9e7928d14bca7ae9cd82b226d790d6e9fe1287ae
Malay-79 <malaypratap0709@gmail.com>
2026-06-19 13:20:14 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abx"
+console.log(type(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"22" => 22
+//"33abc" => NaN
+//true =>1; false=>0 vice versa
+//""=>false in bool
+//"asc" => true in bool
+
